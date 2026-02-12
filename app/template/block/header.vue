d10de7c2c8dbdc22c9cc2cd4d21285da37a54fe0
dreamerview <temirkhan.onyx@gmail.com>
2026-02-12 13:29:01 +0500

    added header sticky

--- app/template/block/header.vue
+++ app/template/block/header.vue
@@ -1,21 +1,23 @@
 <template>
-    <header class="container my-4">
-        <div class="row align-items-center">
-            <div class="col-md-2 col-6 order-md-1 order-1">
-                <img src="/logo.png" style="max-width:100px;width:100%;" alt=""/>
-            </div>
-            <div class="col-md-8 order-md-2 order-3 mt-4 overflow-x-auto">
-                <div class="d-flex align-items-sm-center justify-content-sm-center gap-5">
-                    <a class="link-secondary text-decoration-none flex-shrink-0" href="">Home</a>
-                    <a class="link-secondary text-decoration-none flex-shrink-0" href="">Services</a>
-                    <a class="link-secondary text-decoration-none flex-shrink-0" href="">About us</a>
-                    <a class="link-secondary text-decoration-none flex-shrink-0" href="">More Links</a>
+    <header class="container-fluid py-3 position-sticky top-0 bg-body" style="z-index:999">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-2 col-6 order-md-1 order-1">
+                    <img loading="lazy" src="/logo.png" style="max-width:100px;width:100%;" alt=""/>
                 </div>
-            </div>
-            <div class="col-md-2 col-6 d-flex justify-content-end order-md-3 order-2">
-                <button class="btn btn-dark rounded-3">
-                    Get Started
-                </button>
+                <div class="col-md-8 order-md-2 order-3 mt-md-0 mt-4 overflow-x-auto h-100">
+                    <div class="d-flex align-items-center justify-content-center gap-5 h-100">
+                        <a class="link-secondary text-decoration-none flex-shrink-0" href="">Home</a>
+                        <a class="link-secondary text-decoration-none flex-shrink-0" href="">Services</a>
+                        <a class="link-secondary text-decoration-none flex-shrink-0" href="">About us</a>
+                        <a class="link-secondary text-decoration-none flex-shrink-0" href="">More Links</a>
+                    </div>
+                </div>
+                <div class="col-md-2 col-6 d-flex justify-content-end order-md-3 order-2">
+                    <button class="btn btn-dark rounded-3">
+                        Get Started
+                    </button>
+                </div>
             </div>
         </div>
     </header>
